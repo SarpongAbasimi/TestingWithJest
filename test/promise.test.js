cae42027ef33be3aa71eca2b2247b3420f087b69
sarpong <abasimisarpong@rocketmail.com>
2019-05-28 22:18:01 +0100

    Able to test normal functions but not asyn methods

--- test/promise.test.js
+++ test/promise.test.js
@@ -1,6 +1,6 @@
 const promise = require('../src/promise');
 
-describe('A funtion that returns a promise',()=>{
+describe('A funtion that returns a promise', ()=>{
   test('it returns an array of objects', ()=>{
   });
 });
