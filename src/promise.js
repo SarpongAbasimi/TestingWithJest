11cffe01930a90cf9e284858bd6dda08c6464959
sarpong <abasimisarpong@rocketmail.com>
2019-07-16 15:14:02 +0100

    updated loadash

--- src/promise.js
+++ src/promise.js
@@ -1,17 +1,11 @@
 const fetch = require("node-fetch");
 
-//How to I test for what is being returned.
-//If stubbing how do I do that.
-//https://jsonplaceholder.typicode.com
-
-
-
-exports.promise = (callback)=>{
-  fetch('https://jsonplaceholder.typicode.com/posts/1')
-  .then(response => response.json())
-  .then(jsonData => callback(jsonData))
-  .catch(error=> error)
-};
+// exports.promise = (callback)=>{
+//   fetch('https://jsonplaceholder.typicode.com/posts/1')
+//   .then(response => response.json())
+//   .then(jsonData => callback(jsonData))
+//   .catch(error=> error)
+// };
 
 
 
--- test/promise.test.js
+++ test/promise.test.js
@@ -1,10 +1,3 @@
 
 const { promise } = require('../src/promise');
 
-describe('A function that returns a promise', ()=>{
-  test('it returns an array of objects', () => {
-    
-  });
-});
-
-// Look into = > https://www.npmjs.com/package/jest-fetch-mock
