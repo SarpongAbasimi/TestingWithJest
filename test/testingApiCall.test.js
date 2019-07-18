d34ca9bcc76d2f060fca58f95e65d83435602a75
sarpong <abasimisarpong@rocketmail.com>
2019-07-18 15:17:20 +0100

    wrote test for the function

--- test/testingApiCall.test.js
+++ test/testingApiCall.test.js
@@ -1,18 +1,54 @@
-const { apiCall } = require('../src/testingApiCall'),
-fetch = require('node-fetch');
+const { apiCall, Users } = require('../src/testingApiCall'),
+fetch = require('node-fetch'),
+axios = require('axios');
 
-jest.mock('node-fetch')
+jest.mock('axios')
+// jest.mock('node-fetch')
+
+// describe('Mocking jsonplaceholder response data', () => {
+//   test('the right data was returned', ()=> {
+//     const responseData = 
+//     { ok: true,
+//       status: 200,
+//       data: [{ id: 1, title: 'delectus aut autem'}]
+//     }
+//     // fetch.mockResolvedValue(responseData)
+//     fetch.mockImplementation(() => Promise.resolve(responseData))
+
+//     return firstApiCall().then(response => {
+
+//     })
+//   })
+// });
+
 
 describe('Mocking jsonplaceholder response data', () => {
   test('the right data was returned', ()=> {
     const responseData = 
     { ok: true,
-      status_code: 200,
+      status: 200,
       data: [{ id: 1, title: 'delectus aut autem'}]
     }
-    fetch.mockResolvedValue(responseData)
-    apiCall().then(response => {
-      expect(response.id).toBe(1)
+    axios.get.mockResolvedValue(responseData)
+
+    return apiCall().then(response => {
+      expect(response[0].id).toEqual(1)
+      expect(response[0].title). toEqual('delectus aut autem')
     })
   })
+});
+
+
+describe('Mocking jsonplaceholder response data', () => {
+  test('should fetch users', () => {
+    const users = [{name: 'Bob'}];
+    const resp = {data: users};
+    axios.get.mockResolvedValue(resp);
+  
+    // or you could use the following depending on your use case:
+    // axios.get.mockImplementation(() => Promise.resolve(resp))
+  
+    return Users.all().then(data => console.log(data));
+  });
+
 });
