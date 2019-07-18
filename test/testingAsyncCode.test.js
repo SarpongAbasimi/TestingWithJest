652b7895512d2f39040112fdc3b7836ca287dca2
sarpong <abasimisarpong@rocketmail.com>
2019-07-18 13:54:12 +0100

    added a secong test to use async await

--- test/testingAsyncCode.test.js
+++ test/testingAsyncCode.test.js
@@ -1,19 +1,17 @@
 const { myAsync, myAsyncPear } = require('../src/testingAsyncCode')
 
-
 describe('Testing an async function using promise', () => {
   test('it resolves to a message', ()=> {
     return myAsync().then(resolvedMessage => {
-      console.log(resolvedMessage)
       expect(resolvedMessage).toEqual('Yes there are 2 apples')
-    })
-  })
+    });
+  });
 });
 
 describe('Testing an asyncPear function using async await', () => {
   test('it resolves to a message', async ()=> {
     let resolvedValue = await myAsyncPear()
     expect(resolvedValue).toEqual('Yes there a pear')
-  })
+  });
 });
 
